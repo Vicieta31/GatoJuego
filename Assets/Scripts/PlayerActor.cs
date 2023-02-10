87051893abad93fe566ddf91cdb5e23369e4821b
Daniel Castillo Romero <daniel.castillo@epia.granollers>
2023-02-10 19:56:36 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour
+{
+
+    public Sprite gfx_Cat;
+    public SpriteRenderer renderer;
+    Rigidbody rig;
+    public float velocity = 0.1f;
+    public int playerIndex = 0;
+    public Camera camera;
+
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gfx_Cat != null && renderer != null)
+        {
+            renderer.sprite = gfx_Cat;
+        }
+        rig = GetComponent<Rigidbody>();
+        if (playerIndex == 1)
+        {
+            camera.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rig.AddForce(Vector2.right * velocity, ForceMode.Impulse);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jump), ForceMode.Impulse);
+        }
+    }
+}

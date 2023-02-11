5349f5195ede722884e6ad2355f6f27cbd41c9c9
Bernat Viñals <bernatvinyals@gmail.com>
2023-02-11 17:09:28 +0100

    Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,20 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void EscenaJuego()
     {
         SceneManager.LoadScene("SampleScene");
     }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }

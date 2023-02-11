f8a25ab49690d8ab28249a48b0de67b42d753291
Daniel Castillo Romero <danicastillor04@gmail.com>
2023-02-11 11:26:41 +0100

    asd

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rig;
+    public float jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jump), ForceMode.Impulse);
+        }
+    }
+}

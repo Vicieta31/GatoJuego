f972857e7f876ac94966e49ebecb3454ef9007bd
Bernat Viñals <bernatvinyals@gmail.com>
2023-02-11 19:00:21 +0100

    Game manager - ui - finishline

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject spawnPoint;
+    public UIManager UIManager;
+    PlayerActor player1;
+    PlayerActor player2;
+    private void Start()
+    {
+        GameStart();
+    }
+    void GameStart()
+    {
+        
+        player1 = Instantiate(playerPrefab, spawnPoint.transform).GetComponent<PlayerActor>();
+        player2 = Instantiate(playerPrefab, new Vector3(
+            spawnPoint.transform.position.x, 
+            spawnPoint.transform.position.y, 
+            spawnPoint.transform.position.z+10f), 
+            spawnPoint.transform.rotation).GetComponent<PlayerActor>();
+        player2.playerIndex = 1;
+    }
+    private void Update()
+    {
+        if (player1.muerto())
+        {
+            //Jugador 1 muerto
+        }
+        if (player2.muerto())
+        {
+            //Jugador 2 muerto
+        }
+        UIManager.SetHpPlayer(0, player1.GetHp());
+        UIManager.SetHpPlayer(1, player2.GetHp());
+
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite hpFull;
+    public Sprite hpNone;
+
+    public Image[] P1_hp;
+    public Image[] P2_hp;
+
+    public void SetHpPlayer(int playerIndex, int hp)
+    {
+        switch (playerIndex)
+        {
+            default:
+                break;
+            case 0:
+                for (int i = 0; i < P1_hp.Length; i++)
+                {
+                    P1_hp[i].sprite = hpFull;
+                    if (i >=hp)
+                    {
+                        P1_hp[i].sprite = hpNone;
+                    }
+                }
+                break;
+            case 1:
+                for (int i = 0; i < P2_hp.Length; i++)
+                {
+                    P2_hp[i].sprite = hpFull;
+                    if (i >= hp)
+                    {
+                        P2_hp[i].sprite = hpNone;
+                    }
+                }
+                break;
+        }
+    }
+    public void ShowGameEndedScreen()
+    {
+
+    }
+
+}
